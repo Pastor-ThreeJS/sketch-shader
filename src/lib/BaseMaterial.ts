a68ba861258f80913a7a044f112c764d56ce1620
川路不可涉 <57129584+Lustianshun@users.noreply.github.com>
2021-11-14 12:46:59 +0800

    test

--- src/lib/BaseMaterial.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-export default abstract class BaseMaterial {
-    material: THREE.ShaderMaterial
-    abstract Init(): void;
-    abstract Update(): void;
-    abstract Resize(): void;
-    abstract Destroyed(): void;
-}
--- src/lib/BaseMesh.ts
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-export default abstract class BaseMesh {
-    mesh: THREE.Mesh
-    abstract Init(param: any): void;
-    abstract Update(): void;
-    abstract Destroyed(): void;
-}
